933212c9e8a4789d1b17f1706d8a1eb5588443a9
Mert Incesu <mert.incesu03@gmail.com>
2024-08-28 10:41:19 +0300

    updated gibberish intent

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def prompt_func(query, n):
             "Greendex is a scoring system that companies fill out a form for. The score is about how environmentally friendly you are. The form that the companies have to fill out have these questions: "
             "1.1 Sirket Adi, 1.2 Sektor, 1.3 Merkez Lokasyonu, 1.4 Calisan Sayisi, 2.1 Birincil Enerji Kaynagi, 2.2 Atik Yonetim Sistemi Varligi, 2.3 Karbon Ayak Izi, 2.4 Su Kullanimi ve koruma uygulamalari, 3.1 Surdurebilirlik Projeleri, 3.2 Cevresel duzenlemelere uyumluluk"
             "Please classify the following query into one of the following categories, taking in consideration what greendex is and how the form is structured: "
-            "'General Greendex Info', 'Greendex Form Specific Inquiry', 'Greeting', 'Gibberish', 'None'"
+            "'General Greendex Info', 'Greendex Form Specific Inquiry', 'Greeting', 'Gibberish(nonsense)', 'None'"
             "Your response should ONLY be one of the categories provided, with no additional words. So your ourput format is only the category from one of the provided ones, with no additonal words"
             "Query: " + query
         )
